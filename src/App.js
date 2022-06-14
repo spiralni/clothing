74d4cc17534edf28b8801115cc6f5f3b273e34a3
Cesar Lopez <clopez@poweredbylogica.com>
2022-06-14 11:44:20 -0600

    some styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Outlet, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import './categories.styles.scss'
 
 import Navigation from './routes/navigation/navigation.component'
