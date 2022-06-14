b3980532a6b0aae7eed958b811e05a23d30187c1
Cesar Lopez <clopez@poweredbylogica.com>
2022-06-14 11:54:35 -0600

    signin component added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './categories.styles.scss'
 
 import Navigation from './routes/navigation/navigation.component'
 import Home from './routes/home/home.component'
+import SignIn from './routes/sign-in/sign-in.component'
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />}></Route>
+        <Route path='sign-in' element={<SignIn />}></Route>
       </Route>
     </Routes>
   )
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -13,7 +13,8 @@ const Navigation = () => {
             <AppLogo className="logo" />
           </Link>
           <div className="nav-links-container">
-            <Link className="nav-link" to='/shop'>Shop</Link>  
+            <Link className="nav-link" to='/shop'>Shop</Link>
+            <Link className="nav-link" to='/sign-in'>Sign-in</Link>
           </div>
         </div>
         <Outlet />
