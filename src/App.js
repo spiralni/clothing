b9d78b5e62a0ca484f3491ae74c4f05ae94f722b
Cesar Lopez <clopez@poweredbylogica.com>
2022-06-13 22:04:05 -0600

    adding routing

--- src/App.js
+++ src/App.js
@@ -1,37 +1,14 @@
+import { Route, Routes } from 'react-router-dom'
 import './categories.styles.scss'
 import Categories from './components/categories/categories.component'
+import Home from './routes/home/home.component'
 
 const App = () => {
-
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ] 
-
-  return <Categories categories={categories}></Categories>
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
+  )
 }
 
 export default App
